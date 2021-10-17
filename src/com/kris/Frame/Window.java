fe2f54c6f3425240e862485003a8501cd6c84d5c
kris <krishnav0110@gmail.com>
2021-10-17 19:41:53 +0530

    separated basic initalization of titlebar and JFrame now sets full window size, also now only one instanceof the window is allowed.

--- src/com/kris/Frame/Window.java
+++ src/com/kris/Frame/Window.java
@@ -1,78 +1,58 @@
 package com.kris.Frame;
 
+import java.awt.GraphicsEnvironment;
 import java.awt.Color;
-import java.awt.Dimension;
 
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.event.MouseInputAdapter;
-import java.awt.event.MouseEvent;
-
-import com.kris.MainEngine.Renderer;
 
 public class Window extends JFrame{
 
-    public JPanel titleBar;
+    private static Window window;
+    public TitleBar titleBar;
+    public JPanel mainArea;
+    public JPanel menu;
 
-    public Window(String title, int width, int height){
+    private Window(String title){
 
-        this.setLocationRelativeTo(null);
-        this.setLocation(0, 0);
-        this.setBackground(Color.WHITE);
-        this.setPreferredSize(new Dimension(width, height));
-        this.setAlwaysOnTop(false);
+        titleBar = new TitleBar(title);
+        mainArea = new JPanel();
+        menu = new JPanel();
 
         this.setTitle(title);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
         this.setUndecorated(true);
+        this.setLayout(null);
 
-        this.pack();
+        this.setBounds(GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds());
+
         this.setVisible(true);
+        init();
     }
 
-    public void createDESApp(Renderer renderer){
+    public static Window createDESApp(){
+
+        if(window == null)
+            window = new Window("Digital Electronics Simulator");
         
-        JPanel mainArea = new JPanel();
-        JPanel menu = new JPanel();
-        JPanel close_btn = new JPanel();
-        titleBar = new JPanel();
+        return window;
+    }
+
+    public void init(){
 
         this.add(titleBar);
         this.add(mainArea);
         this.add(menu);
 
-        titleBar.add(close_btn);
-        mainArea.add(renderer);
-
-        titleBar.setBackground(Color.DARK_GRAY);
-        titleBar.setBounds(0, 0, this.getWidth(), 30);
-        
-
-        close_btn.setBackground(titleBar.getBackground());
-        close_btn.setBounds(titleBar.getWidth() - 40, 0, 40, titleBar.getHeight());
+        titleBar.init();
 
         mainArea.setBackground(Color.BLACK);
         mainArea.setBounds(0, 30, (int) (this.getWidth() * 0.8), this.getHeight() - 30);
 
         menu.setBackground(Color.GRAY);
         menu.setBounds((int) (this.getWidth() * 0.8), 30, (int) (this.getWidth() * 0.2), this.getHeight() - 30);
-
-        renderer.setBounds(0, 0, mainArea.getWidth(), mainArea.getHeight());
-
-        close_btn.addMouseListener(new MouseInputAdapter(){
-            
-            @Override
-            public void mouseEntered(MouseEvent e){
-
-                close_btn.setBackground(Color.RED);
-            }
-
-            @Override
-            public void mouseExited(MouseEvent e){
-
-                close_btn.setBackground(titleBar.getBackground());
-            }
-        });
+        menu.setBorder(BorderFactory.createLineBorder(Color.BLACK, 5));
     }
 }
